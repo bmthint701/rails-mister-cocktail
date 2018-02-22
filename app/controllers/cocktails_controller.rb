9cc71e02a7ed89e77cc48c2262770629ffb77247
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-22 15:49:18 +0900

    got the crazy add thing to work

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,4 +5,12 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+  def new
+    @cocktail = Cocktail.new
+  end
+  def create
+    @cocktail = Cocktail.create(name: params["cocktail"]["name"])
+    Dose.create(description: params["cocktail"]["doses"]["description"], ingredient_id: params["cocktail"]["doses"]["ingredient_id"], cocktail_id: @cocktail.id)
+    redirect_to cocktail_path(@cocktail)
+  end
 end
